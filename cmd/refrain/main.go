4dff32bc98bd24cae08d2d3024cba3b874f2c703
Gérald Leban <gerald.leban0@gmail.com>
2026-02-08 11:38:56 +0100

    feat: implement graceful shutdown with signal handling

--- cmd/refrain/main.go
+++ cmd/refrain/main.go
@@ -1,4 +1,28 @@
 package main
 
+import (
+	"context"
+	"log/slog"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
 func main() {
+	ctx, cancel := mainContext(context.Background())
+	defer cancel()
+	slog.Info("Starting Refrain...")
+
+	<-ctx.Done()
+	slog.Info("Shutting down...")
+}
+
+// mainContext returns a context that is cancelled when the process receives a signal to exit.
+func mainContext(ctx context.Context) (context.Context, context.CancelFunc) {
+	return signal.NotifyContext(ctx,
+		os.Interrupt,
+		syscall.SIGHUP,
+		syscall.SIGTERM,
+		syscall.SIGABRT,
+	)
 }
